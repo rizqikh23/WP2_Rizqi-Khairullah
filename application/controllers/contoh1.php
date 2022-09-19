11955d6dfb9b7fed503ada36a71da135da13c66b
rizqikh23 <khairullahrizqi@gmail.com>
2022-09-19 19:51:29 +0700

    Rizqi Khairullah Tugas 1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Rizqi Khairullah
+ Saya tinggal di kota Pontianak
+ Hobi saya adalah bermain Game";
+ }
+}
