ca8b21881dc62f625a859c43200fb791996ae2bc
rizqikh23 <khairullahrizqi@gmail.com>
2022-09-24 16:46:54 +0700

    tes

--- application/controllers/latihan1.php
+++ application/controllers/latihan1.php
@@ -9,7 +9,7 @@ class Latihan1 extends CI_Controller
     public function penjumlahan($n1, $n2)
      {
         $this->load->model('model_latihan1');
-         $hasil = $this->model_latihan1->jumlah(2, 6);
+         $hasil = $this->model_latihan1->jumlah($n1, $n2);
         echo "Hasil Penjumlahan dari ". $n1 ." + ". $n2 ." = "
         .$hasil;
  }
