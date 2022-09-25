d89fa1f7e0cedbdda6063a4d3b5f72fa81223a35
rizqikh23 <khairullahrizqi@gmail.com>
2022-09-25 12:05:57 +0700

    tes

--- application/models/model_latihan1.php
+++ application/models/model_latihan1.php
@@ -4,7 +4,7 @@ class model_latihan1 extends CI_Model
  //membuat variable untuk menampung nilai
     public $nilai1, $nilai2, $hasil;
  //method penjumlahan
-    public function jumlah($n1 = null, $n2 = null)
+    public function jumlah($nil1 = null, $nil2 = null)
     {
         $this->nilai1 = $nil1;
         $this->nilai2 = $nil2;
