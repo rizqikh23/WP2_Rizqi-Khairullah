c797f6df1a9bafa2dda4362581bba60fa3309220
rizqikh23 <khairullahrizqi@gmail.com>
2022-10-02 12:04:53 +0700

    tes

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -9,7 +9,7 @@ class Web extends CI_Controller
 
     public function index()
     {
-        $data['judul'] = “Halaman Depan”;
+        $data['judul'] = "Halaman Depan";
         $this->load->view('v_header', $data);
         $this->load->view('v_index', $data);
         $this->load->view('v_footer', $data);
