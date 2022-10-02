1ca205756d9aa859ed9076248873534d6ad183ff
rizqikh23 <khairullahrizqi@gmail.com>
2022-10-02 12:37:39 +0700

    tes

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,18 +2,18 @@
     <h1><?php echo $judul ?></h1>
     <h4>Nama</h4>
     <ul type="disc">
-        <li>Nama Depan : Imam</li>
-        <li>Nama Belakang : Nawawi</li>
+        <li>Nama Depan : Rizqi</li>
+        <li>Nama Belakang : Khairullah</li>
     </ul>
     <br>
     <h4>Alamat</h4>
     <ul type="none">
-        <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>
+        <li> Jalan P. Natakusuma Gg. Langgar H. Ali No. 10B</li>
     </ul>
     <br>
     <h4>Tempat Lahir</h4>
     <ul type="none">
-        <li>Cirebon</li>
+        <li>Bogor</li>
     </ul>
     <br>
     <h4>Olah Raga Favorit</h4>
