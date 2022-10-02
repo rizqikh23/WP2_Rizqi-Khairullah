f6b38963d9417efac135ea4f0bdf225cd53bdfac
rizqikh23 <khairullahrizqi@gmail.com>
2022-10-02 11:46:16 +0700

    Tugas Pertemuan 3

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -2,9 +2,10 @@
 defined(‘BASEPATH’) or exit (‘no direct script access allowed’);
 class Web extends CI_Controller
 {
-    function __construct(){
+    function __construct()
+    {
         parent::__construct();
-
+        $this->load->helper('url');
     }
 
     public function index()
