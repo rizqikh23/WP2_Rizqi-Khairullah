c92114c26a5e02903e3b1dfebc21aac84e8e3eae
rizqikh23 <khairullahrizqi@gmail.com>
2022-10-02 12:03:41 +0700

    tes

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -1,5 +1,5 @@
 <?php
-defined (‘BASEPATH’) or exit ('No direct script access allowed’);
+defined ('BASEPATH') or exit ('No direct script access allowed');
 class Web extends CI_Controller
 {
     function __construct()
@@ -9,9 +9,9 @@ class Web extends CI_Controller
 
     public function index()
     {
-        $data[‘judul’] = “Halaman Depan”;
-        $this->load->view(‘v_header’, $data);
-        $this->load->view(‘v_index’, $data);
-        $this->load->view(‘v_footer’, $data);
+        $data['judul'] = “Halaman Depan”;
+        $this->load->view('v_header', $data);
+        $this->load->view('v_index', $data);
+        $this->load->view('v_footer', $data);
     }
 }
