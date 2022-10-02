59235aa5de66e93650c3deee7e9128bd9bdcf12f
rizqikh23 <khairullahrizqi@gmail.com>
2022-10-02 12:00:39 +0700

    tes

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -1,5 +1,5 @@
 <?php
-defined (‘BASEPATH’) or exit (No direct script access allowed’);
+defined (‘BASEPATH’) or exit ('No direct script access allowed’);
 class Web extends CI_Controller
 {
     function __construct()
