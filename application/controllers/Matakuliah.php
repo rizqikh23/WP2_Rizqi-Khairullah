78318fb500411bdada754898d03f4d9628b7ea8d
rizqikh23 <khairullahrizqi@gmail.com>
2022-10-03 20:16:15 +0700

    Tugas Pertemuan 4

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -9,13 +9,13 @@ class Matakuliah extends CI_Controller
     {
         $this->form_validation->set_rules('kode', 'Kode Matakuliah', 
             'required|min_length[3]', [
-            'required' => 'Kode Matakuliah Harus diisi',
-            'min_lenght' => 'Kode terlalu pendek'
+            'required' => "Kode Matakuliah Harus diisi",
+            'min_lenght' => "Kode terlalu pendek"
         ]);
         $this->form_validation->set_rules('nama', 'Nama Matakuliah', 
             'required|min_length[3]', [
-            'required' => 'Nama Matakuliah Harus diisi',
-            'min_lenght' => 'Nama terlalu pendek'
+            'required' => "Nama Matakuliah Harus diisi",
+            'min_lenght' => "Nama terlalu pendek"
         ]);
         if ($this->form_validation->run() != true) {
             $this->load->view('view-form-matakuliah');
