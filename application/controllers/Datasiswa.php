9465ca4f1763cc76c49a9ede3da75c75f652e9a8
rizqikh23 <khairullahrizqi@gmail.com>
2022-10-17 21:00:53 +0700

    Tugas Pertemuan 6

---
+++ application/controllers/Datasiswa.php
@@ -0,0 +1,56 @@
+<?php
+class Matakuliah extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('view-form-matakuliah');
+    }
+    public function cetak()
+    {
+        $this->form_validation->set_rules('nama', 'Nama Siswa', 
+            'required|min_length[3]', [
+            'required' => 'Nama Siswa Harus diisi',
+            'min_lenght' => 'Nama Siswa terlalu pendek'
+        ]);
+        $this->form_validation->set_rules('nis', 'NIS', 
+            'required|min_length[3]', [
+            'required' => 'NIS Harus diisi',
+            'min_lenght' => 'NIS terlalu pendek'
+        ]);
+        $this->form_validation->set_rules('kelas', 'Kelas', 
+        'required|min_length[3]', [
+        'required' => 'Kelas Harus diisi',
+        'min_lenght' => 'Kelas terlalu pendek'
+        ]);
+        $this->form_validation->set_rules('tanggallahir', 'Tanggal Lahir', 
+        'required|min_length[3]', [
+        'required' => 'Tanggal Lahir Harus diisi',
+        'min_lenght' => 'Tanggal Lahir terlalu pendek'
+        ]);
+        $this->form_validation->set_rules('tempatlahir', 'Tempat Lahir', 
+        'required|min_length[3]', [
+        'required' => 'Tempat Lahir Harus diisi',
+        'min_lenght' => 'Tempat Lahir terlalu pendek'
+        ]);
+        $this->form_validation->set_rules('alamat', 'Alamat', 
+        'required|min_length[3]', [
+        'required' => 'Alamat Harus diisi',
+        'min_lenght' => 'Alamat terlalu pendek'
+        ]);
+        if ($this->form_validation->run() != true) {
+            $this->load->view('view-form-siswa');
+        } else {
+            $data = [
+                'nama' => $this->input->post('nama'),
+                'nis' => $this->input->post('nis'),
+                'kelas' => $this->input->post('kelas')
+                'tanggallahir' => $this->input->post('tanggallahir')
+                'tempatlahir' => $this->input->post('tempatlahir')
+                'alamat' => $this->input->post('alamat')
+                'jeniskelamin' => $this->input->post('jeniskelamin')
+                'agama' => $this->input->post('agama')
+            ];
+            $this->load->view('view-data-siswa', $data);
+        }
+    }
+}
---
+++ application/views/view-form-siswa.php
@@ -0,0 +1,91 @@
+<html>
+<head>
+    <title>Form Input Data Siswa</title>
+</head>
+<body>
+    <center>
+        <form action="<?= base_url('datasiswa/cetak'); ?>"
+        method="post">
+            <table>
+                <tr>
+                    <th colspan="3">
+                    Form Input Data Siswa
+                    </th>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <hr>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Nama Siswa</th>
+                    <th>:</th>
+                    <td>
+                        <input type="text" name="nama" id="nama">
+                    </td>
+                </tr>
+                <tr>
+                    <th>NIS</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="nis" id="nis">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Kelas</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="kelas" id="kelas">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Tanggal Lahir</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="tanggallahir" id="tanggallahir">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Tempat Lahir</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="tempatlahir" id="tempatlahir">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Alamat</th>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="alamat" id="alamat">
+                    </td>
+                </tr>
+                <form action="view-form-siswa.php"></form> <p>Jenis Kelamin</p>
+                <input type="radio" id="laki" name="jeniskelamin" value="Laki-Laki">
+                <label for="laki">Laki-Laki</label><br>
+                <input type="radio" id="perempuan" name="jeniskelamin" value="Perempuan">
+                <label for="perempuan">Perempuan</label>
+                <tr>
+                    <th>Agama</th>
+                    <td>:</td>
+                    <td>
+                        <select name="agama" id="agama">
+                            <option value="">Pilih Agama</option>
+                            <option value="2">Islam</option>
+                            <option value="3">Kristen Katholik</option>
+                            <option value="4">Kristen Protestan</option>
+                            <option value="5">Budha</option>
+                            <option value="6">Hindu</option>
+                            <option value="7">Khonghucu</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3" align="center">
+                        <input type="submit" value="Submit">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </center>
+</body>
+</html>
